a5db101d7e99c8b7af6f19dd45e01d41a6e805f3
BotalovSerg <79372024+BotalovSerg@users.noreply.github.com>
2022-02-01 21:34:46 +0500

    Add files via upload

--- pack_geomet.py
+++ pack_geomet.py
@@ -1,39 +1,28 @@
 import tkinter as tk
-from tkinter import ttk
-
-root = tk.Tk()
-root.title('Temperature converter')
-root.resizable(False, False)
-
-celsius_value = tk.StringVar()
-fahrenheit_value = tk.StringVar(value='Temperature in fahrenheit...')
 
 
-def convert(*args):
-    try:
-        celsius = float(celsius_value.get())
-        fahrenheit = celsius * 9 / 5 + 32
-        fahrenheit_value.set(f'{fahrenheit:.3f}')
-    except ValueError:
-        pass
+def increase():
+    value = int(lbl_value["text"])
+    lbl_value["text"] = f"{value + 1}"
 
 
-main_frame = ttk.Frame(root, padding=(20, 10))
-main_frame.grid()
+def decrease():
+    value = int(lbl_value["text"])
+    lbl_value["text"] = f"{value - 1}"
 
-celsius_label = ttk.Label(main_frame, text='Celsius: ')
-celsius_entry = ttk.Entry(main_frame, width=10, textvariable=celsius_value)
-fahrenheit_label = ttk.Label(main_frame, text='Fahrenheit: ')
-f_display_label = ttk.Label(main_frame, textvariable=fahrenheit_value)
-convert_button = ttk.Button(main_frame, text='Convert', command=convert)
 
-celsius_label.grid(row=0, column=0, sticky='W', padx=5, pady=5)
-celsius_entry.grid(row=0, column=1, sticky='WE', padx=5, pady=5)
-celsius_entry.focus()
-fahrenheit_label.grid(row=1, column=0, sticky='W', padx=5, pady=5)
-f_display_label.grid(row=1, column=1, sticky='WE', padx=5, pady=5)
-convert_button.grid(row=2, column=0, columnspan=2, sticky='WE', padx=5, pady=5)
+window = tk.Tk()
 
-root.bind('<Return>', convert)
+window.rowconfigure(0, minsize=50, weight=1)
+window.columnconfigure([0, 1, 2], minsize=50, weight=1)
 
-root.mainloop()
+btn_decrease = tk.Button(master=window, text="-", command=decrease)
+btn_decrease.grid(row=0, column=0, sticky="nsew")
+
+lbl_value = tk.Label(master=window, text="0")
+lbl_value.grid(row=0, column=1)
+
+btn_increase = tk.Button(master=window, text="+", command=increase)
+btn_increase.grid(row=0, column=2, sticky="nsew")
+
+window.mainloop()
